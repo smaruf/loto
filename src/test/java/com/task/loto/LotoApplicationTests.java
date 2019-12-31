81f08b643033fd6ba2b5e042697c90b91e527d81
Muhammad Shamsul Maruf <smaruf00ruet320@gmail.com>
2019-12-31 14:04:48 +0600

    Create context test 2019.12.31.1404

---
+++ src/test/java/com/task/loto/LotoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.loto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LotoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
